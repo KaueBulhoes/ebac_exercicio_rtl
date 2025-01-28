b40f26b3cba737868a752864cba6abb2a2d75cce
Kaue <130503651+KaueBulhoes@users.noreply.github.com>
2025-01-28 17:34:05 -0300

    feat: add tests

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -3,8 +3,30 @@ import Post from '.';
 import PostComment from '.';
 
 describe('Teste para o componente PostComment', () => {
-    it('Deve renderizar o componente corretamente', () => {
+    test('Deve renderizar o componente corretamente', () => {
         render(<PostComment/>);
         expect(screen.getByText('Comentar')).toBeInTheDocument();
     });
+
+    test("Deve adicionar dois comentarios", () => {
+        render(<PostComment/>)
+
+        fireEvent.change(screen.getByTestId('teste-comentario'), {
+            target: {
+                value: 'Primeiro comentario do teste'
+            }
+        })
+        
+        fireEvent.click(screen.getByTestId('botao-adicionar'))
+
+        fireEvent.change(screen.getByTestId('teste-comentario'), {
+            target: {
+                value: 'Segundo comentario do teste'
+            }
+        })
+
+        fireEvent.click(screen.getByTestId('botao-adicionar'))
+
+        expect(screen.getAllByTestId('teste-elemento')).toHaveLength(2)
+    })
 });
